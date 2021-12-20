4698ec1f98010c030faf27747f90a8c5e05fa4cb
Pasindu <pasindudananjaya660@gmail.com>
2021-12-20 21:25:45 +0530

    Initialize project

---
+++ src/main/java/com/gwuim/lang/GwuimApplication.java
@@ -0,0 +1,13 @@
+package com.gwuim.lang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwuimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GwuimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gwuim/lang/GwuimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwuim.lang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwuimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
